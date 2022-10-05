ec506900cc2c4d330f749ee9f08e4fc3dfc5b25f
kilian stisser <stisser@neoskop.de>
2022-10-05 13:40:06 +0200

    fix event handling

--- template/nestjs/function/src/main.ts
+++ template/nestjs/function/src/main.ts
@@ -1,7 +1,7 @@
 export default async (event: any, context: any, _cb: any) => {
   const result = {
     body: JSON.stringify(event.body),
-    "content-type": event.headers["content-type"],
+    'content-type': event.headers['content-type'],
   };
 
   return context.status(200).succeed(result);
--- template/nestjs/index.ts
+++ template/nestjs/index.ts
@@ -2,15 +2,15 @@
 // Copyright (c) OpenFaaS Author(s) 2021. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import express from "express";
+import express from 'express';
 const app = express();
-import handler from "./function/handler.js";
-import bodyParser from "body-parser";
-import { isStream } from "is-stream";
+import main from './function/src/main.js';
+import bodyParser from 'body-parser';
+import { isStream } from 'is-stream';
 
-const defaultMaxSize = "100kb"; // body-parser default
+const defaultMaxSize = '100kb'; // body-parser default
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 const rawLimit = process.env.MAX_RAW_SIZE || defaultMaxSize;
 const jsonLimit = process.env.MAX_JSON_SIZE || defaultMaxSize;
@@ -19,16 +19,16 @@ app.use(function addDefaultContentType(req, _res, next) {
   // When no content-type is given, the body element is set to
   // nil, and has been a source of contention for new users.
 
-  if (!req.headers["content-type"]) {
-    req.headers["content-type"] = "text/plain";
+  if (!req.headers['content-type']) {
+    req.headers['content-type'] = 'text/plain';
   }
   next();
 });
 
-if (process.env.RAW_BODY === "true") {
-  app.use(bodyParser.raw({ type: "*/*", limit: rawLimit }));
+if (process.env.RAW_BODY === 'true') {
+  app.use(bodyParser.raw({ type: '*/*', limit: rawLimit }));
 } else {
-  app.use(bodyParser.text({ type: "text/*" }));
+  app.use(bodyParser.text({ type: 'text/*' }));
   app.use(bodyParser.json({ limit: jsonLimit }));
   app.use(bodyParser.urlencoded({ extended: true }));
 }
@@ -96,7 +96,7 @@ class FunctionContext {
 
   fail(value) {
     let message;
-    if (this.status() == "200") {
+    if (this.status() == '200') {
       this.status(500);
     }
 
@@ -110,31 +110,23 @@ const middleware = async (req, res) => {
     if (err) {
       console.error(err);
 
-      return res
-        .status(fnContext.status())
-        .send(err.toString ? err.toString() : err);
+      return res.status(fnContext.status()).send(err.toString ? err.toString() : err);
     }
 
     if (isStream(functionResult)) {
       res.set(fnContext.headers()).status(fnContext.status());
       functionResult.pipe(res);
     } else if (isArray(functionResult) || isObject(functionResult)) {
-      res
-        .set(fnContext.headers())
-        .status(fnContext.status())
-        .send(JSON.stringify(functionResult));
+      res.set(fnContext.headers()).status(fnContext.status()).send(JSON.stringify(functionResult));
     } else {
-      res
-        .set(fnContext.headers())
-        .status(fnContext.status())
-        .send(functionResult);
+      res.set(fnContext.headers()).status(fnContext.status()).send(functionResult);
     }
   };
 
   const fnEvent = new FunctionEvent(req);
   const fnContext = new FunctionContext(cb);
 
-  Promise.resolve(handler(fnEvent, fnContext, cb))
+  Promise.resolve(main(fnEvent, fnContext, cb))
     .then((res) => {
       if (!fnContext.cbCalled) {
         fnContext.succeed(res);
@@ -145,12 +137,12 @@ const middleware = async (req, res) => {
     });
 };
 
-app.post("/*", middleware);
-app.get("/*", middleware);
-app.patch("/*", middleware);
-app.put("/*", middleware);
-app.delete("/*", middleware);
-app.options("/*", middleware);
+app.post('/*', middleware);
+app.get('/*', middleware);
+app.patch('/*', middleware);
+app.put('/*', middleware);
+app.delete('/*', middleware);
+app.options('/*', middleware);
 
 const port = process.env.http_port || 3000;
 
